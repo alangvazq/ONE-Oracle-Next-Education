828d7d4c82aefcea4eee18a8bcd9341ef211313b
alangvazq <alan.g.vazq@gmail.com>
2024-01-25 14:00:40 -0600

    Challenge 03 completed

---
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/Challenge03/app.js
@@ -0,0 +1,33 @@
+/*
+*Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+*/
+
+//  let contador = 1;
+
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++
+// }
+
+/*
+*Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+*/
+
+// let contador = 10;
+
+// while(contador >= 0){
+//     console.log(contador);
+//     contador--
+// }
+
+/* 
+* Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+*/
+
+// let numeroInicial = prompt('Ingresa número');
+// let contador = 0;
+
+// while(contador <= numeroInicial){
+//     console.log(contador);
+//     contador++
+// }
--- 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
@@ -1,19 +1,30 @@
-// Variables
+// * Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let intentos = 1;
+let maximosIntentos = 3;
 
-console.log(numeroUsuario);
+while(numeroUsuario != numeroSecreto){
+    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    console.log(numeroUsuario);
 
-/*
-Esto codigo
-realiza la coparación
-*/
-
-if (numeroUsuario == numeroSecreto) {
-    // Acertamos
-    alert(`Acertaste el número es: ${numeroUsuario}`);
-} else{
-    // No acertamos
-    alert('Lo siento, no acertaste el número')
+    /*
+    * Esto codigo realiza la coparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        // * Acertamos
+        alert(`Acertaste el número es: ${numeroUsuario}. Intentos: ${intentos}`);
+    } else{
+        if (numeroUsuario > numeroSecreto){
+            alert('Número es menor')
+        }else{
+            alert('Número es mayor')
+        }
+        intentos++;
+        if (intentos > maximosIntentos){
+            alert(`Legaste al máximo número de ${maximosIntentos} intentos.`)
+            break;
+        }
+        // * No acertamos
+        // alert('Lo siento, no acertaste el número')
+    }
 }
-
