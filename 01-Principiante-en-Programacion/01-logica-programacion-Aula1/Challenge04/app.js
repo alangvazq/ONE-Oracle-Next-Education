87658ef127a8657b6fda061d5dd4fd29417bd260
alangvazq <alan.g.vazq@gmail.com>
2024-01-25 17:34:58 -0600

    Challenge 04 completed

---
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/Challenge04/app.js
@@ -0,0 +1,80 @@
+// TODO Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
+// console.log("Bienvenido");
+
+// TODO Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+// let nombreA = "Alan"
+// console.log(`¡Hola, ${nombreA}!`);
+
+// TODO Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
+// let nombreB = "Alan"
+// alert(nombreB)
+
+// TODO Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
+// let pregunta = prompt('¿Cuál es el lenguaje de programación que más te gusta?')
+// console.log(pregunta);
+
+// TODO Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+// let valor1 = 0;
+// let valor2 = 0;
+// let resultado = valor1 + valor2;
+
+// console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`);
+
+// TODO Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la resta de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La diferencia entre [valor1] y [valor2] es igual a [resultado]." en la consola.
+// let valor1 = 0;
+// let valor2 = 0;
+// let resultado = valor1 - valor2;
+
+// console.log(`La diferencia entre ${valor1} y ${valor2} es igual a ${resultado}.`);
+
+// TODO Pide al usuario que ingrese su edad con prompt. Con base en la edad ingresada, utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje apropiado en la consola.
+// let edad = prompt('Ingresa edad: ')
+
+// if(edad > 18){
+//     console.log('Mayor de edad.');
+// }else{
+//     console.log('Menor de edad.');
+// }
+
+// TODO Crea una variable "numero" y solicita un valor con prompt. Luego, verifica si es positivo, negativo o cero utilizando un if-else y muestra el mensaje correspondiente.
+// let numero = prompt('Ingresa un número: ');
+
+// if(numero > 0){
+//     console.log('Positivo');
+// }else if(numero < 0){
+//     console.log('Negativo');
+// }else{
+//     console.log('0');
+// }
+
+// TODO Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
+// let contador = 1;
+
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+
+// TODO Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
+// let nota = 0;
+
+// if(nota >= 7){
+//     console.log('Aprobado');
+// }else{
+//     console.log('Reprobado');
+// }
+
+// TODO Utiliza Math.random para generar cualquier número aleatorio y muestra ese número en la consola.
+// let randomNumber = Math.random();
+
+// console.log(randomNumber);
+
+// TODO Utiliza Math.random para generar un número entero entre 1 y 10 y muestra ese número en la consola.
+// let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+// console.log(randomNumber);
+
+// TODO Utiliza Math.random para generar un número entero entre 1 y 1000 y muestra ese número en la consola.
+let randomNumber = Math.floor(Math.random() * 1000 + 1);
+
+console.log(randomNumber);
--- 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
@@ -1,10 +1,11 @@
 // * Variables
-let numeroSecreto = 6;
+let numeroUsuario = 0;
+let numeroSecreto = Math.floor(Math.random()*10+1);
 let intentos = 1;
 let maximosIntentos = 3;
 
 while(numeroUsuario != numeroSecreto){
-    let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    let numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
     console.log(numeroUsuario);
 
     /*
@@ -12,7 +13,7 @@ while(numeroUsuario != numeroSecreto){
     */
     if (numeroUsuario == numeroSecreto) {
         // * Acertamos
-        alert(`Acertaste el número es: ${numeroUsuario}. Intentos: ${intentos}`);
+        alert(`Acertaste el número es: ${numeroUsuario}. Lo hiciste en ${intentos}${intentos == 1 ? ' intento' : ' intentos'}` );
     } else{
         if (numeroUsuario > numeroSecreto){
             alert('Número es menor')
