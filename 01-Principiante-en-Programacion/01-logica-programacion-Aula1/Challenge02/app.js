66d751d3c3e04a79379feb667811a791a1d2707d
alangvazq <alan.g.vazq@gmail.com>
2024-01-25 00:35:45 -0600

    Challenge 2 completed

---
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/Challenge02/app.js
@@ -0,0 +1,47 @@
+// Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo",
+// muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+
+// let diaSemana = prompt('Qué día de la semana es: ');
+
+// if (diaSemana == "Sábado" || diaSemana == "Domingo"){
+//     console.log('¡Buen fin de semana!');
+// } else {
+//     console.log('¡Buena semana!');
+// }
+
+// Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+
+// let number = prompt('Ingresa un número: ');
+
+// if (number > 0){
+//     alert('Positivo')
+// } else if (number < 0){
+//     alert('Negativo')
+// } else{
+//     alert('0')
+// }
+
+// Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100,
+// muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intenta nuevamente para ganar.".
+
+// let score = 800;
+
+// if (score >= 100){
+//     console.log('¡Felicidades, has ganado!');
+// }else{
+//     console.log('Intenta nuevamente para ganar.');
+// }
+
+// Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template
+// string para incluir el valor del saldo.
+
+// let saldo = 100;
+
+// console.log(`Saldo: ${saldo}`);
+
+// Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta
+// de bienvenida usando ese nombre.
+
+let userName = prompt('Ingresa nombre: ');
+
+alert(`Bienvenido ${userName}`)
--- 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
+++ 01-Principiante-en-Programacion/01-logica-programacion-Aula1/app.js
@@ -1,9 +1,19 @@
+// Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor:");
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
 
+/*
+Esto codigo
+realiza la coparación
+*/
+
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
+    // Acertamos
+    alert(`Acertaste el número es: ${numeroUsuario}`);
+} else{
+    // No acertamos
+    alert('Lo siento, no acertaste el número')
 }
 
