73b0c22902041e6a12e56282e8f2d78f2ad4bf78
alangvazq <alan.g.vazq@gmail.com>
2024-06-29 01:49:11 -0600

    Continuación de la parte 3

--- 02-Front-End/04-JavaScript-DOM/script.js
+++ 02-Front-End/04-JavaScript-DOM/script.js
@@ -1,13 +1,62 @@
 const html = document.querySelector('html');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+const titutlo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica'); 
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+
+musica.loop = true;
+
+inputEnfoqueMusica.addEventListener('change', ()=>{
+    if (musica.paused) {
+        musica.play()
+    }else{
+        musica.paused()
+    }
+})
 
 botonCorto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-corto')
+    cambiarContexto('descanso-corto')
+    botonCorto.classList.add('active')
 })
 
 botonEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'enfoque')
+    cambiarContexto('enfoque')
+    botonEnfoque.classList.add('active')
 })
 
-console.log('hola');
+botonLargo.addEventListener('click', () => {
+    cambiarContexto('descanso-largo')
+    botonLargo.classList.add('active')
+})
+
+function cambiarContexto(contexto) {
+    botones.forEach(boton => {
+        boton.classList.remove('active')
+    })
+
+
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `./imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case "enfoque":
+            titutlo.innerHTML = `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`;
+            break;
+        case "descanso-corto":
+            titutlo.innerHTML = `¿Qué tal tomar un respiro?,<br>
+                    <strong class="app__title-strong">¡Haz una pausa corta!</strong>`;
+            break;
+        case "descanso-largo":
+            titutlo.innerHTML = `Hora de volver a la superficie,<br>
+                        <strong class="app__title-strong">Haz una pausa larga.</strong>`;
+            break;
+
+        default:
+            break;
+    }
+}
